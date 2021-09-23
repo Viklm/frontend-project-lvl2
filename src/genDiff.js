c826377c1395ab2ff76eba81cf92a7a278f94116
Viktor <vik333444@yandex.ru>
2021-09-23 10:26:47 +0400

    update

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,11 +3,11 @@ import { readFileSync } from 'fs';
 
 const letCompare = (file1, file2, keys) => {
   const result = {};
-  for (const key of keys) {
+  keys.map((key) => {
     if (_.has(file1, key) && _.has(file2, key)) {
       if (file1[key] === file2[key]) {
         result[key] = file1[key];
-      } 
+      }
       if (file1[key] !== file2[key]) {
         result[`- ${key}`] = file1[key];
         result[`+ ${key}`] = file2[key];
@@ -15,19 +15,18 @@ const letCompare = (file1, file2, keys) => {
     }
     if (_.has(file1, key) && !_.has(file2, key)) {
       result[`- ${key}`] = file1[key];
-    } 
+    }
     if (!_.has(file1, key) && _.has(file2, key)) {
       result[`+ ${key}`] = file2[key];
     }
-  }
+  });
   return result;
 };
 
 const genDiff = (filepath1, filepath2) => {
   const contentOfFile1 = JSON.parse(readFileSync(filepath1));
   const contentOfFile2 = JSON.parse(readFileSync(filepath2));
-  const keysOfBothFiles = [Object.keys(contentOfFile1), Object.keys(contentOfFile2)];
-  const sortedKeysOfBothFiles = _.uniq(keysOfBothFiles.flat());
+  const sortedKeysOfBothFiles = _.union(Object.keys(contentOfFile1), Object.keys(contentOfFile2));
   return letCompare(contentOfFile1, contentOfFile2, sortedKeysOfBothFiles);
 };
 
