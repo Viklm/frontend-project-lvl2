23aad3cfc0303679c585f4bb308c45d8cc9b329f
Viktor <vik333444@yandex.ru>
2021-09-24 14:54:25 +0400

    overwritten the function with the .reduce method. made finding a path into a separate function.

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,33 +1,33 @@
 import _ from 'lodash';
 import { readFileSync } from 'fs';
+import path from 'path';
 
-const letCompare = (file1, file2, keys) => {
-  const result = {};
-  keys.map((key) => {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] === file2[key]) {
-        result[key] = file1[key];
-      }
-      if (file1[key] !== file2[key]) {
-        result[`- ${key}`] = file1[key];
-        result[`+ ${key}`] = file2[key];
-      }
-    }
-    if (_.has(file1, key) && !_.has(file2, key)) {
-      result[`- ${key}`] = file1[key];
-    }
-    if (!_.has(file1, key) && _.has(file2, key)) {
-      result[`+ ${key}`] = file2[key];
-    }
-  });
-  return result;
+const getPath = (filepath) => {
+  const pathToFile = path.resolve(process.cwd(), filepath);
+  const data = JSON.parse(readFileSync(pathToFile, 'utf-8'));
+  return data;
 };
 
 const genDiff = (filepath1, filepath2) => {
-  const contentOfFile1 = JSON.parse(readFileSync(filepath1));
-  const contentOfFile2 = JSON.parse(readFileSync(filepath2));
-  const sortedKeysOfBothFiles = _.union(Object.keys(contentOfFile1), Object.keys(contentOfFile2));
-  return letCompare(contentOfFile1, contentOfFile2, sortedKeysOfBothFiles);
+  const file1 = getPath(filepath1);
+  const file2 = getPath(filepath2);
+  const keys = _.union(Object.keys(file1), Object.keys(file2));
+  const diff = keys.reduce((acc, key) => {
+    if (_.has(file1, key) && _.has(file2, key)) {
+      if (file1[key] === file2[key]) {
+        return `${acc}  ${key}: ${file1[key]}\n`;
+      }
+      return `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
+    }
+    if (_.has(file1, key) && !_.has(file2, key)) {
+      return `${acc}  - ${key}: ${file1[key]}\n`;
+    }
+    if (!_.has(file1, key) && _.has(file2, key)) {
+      return `${acc}  + ${key}: ${file2[key]}\n`;
+    }
+    return acc;
+  }, '');
+  return `{\n${diff}}`;
 };
 
 export default genDiff;
