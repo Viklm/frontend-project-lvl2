ce67e0da140d543c4c412977a4166770845e45a9
Viktor <vik333444@yandex.ru>
2021-10-04 12:21:37 +0400

    added stylish default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import genDiff from '../src/genDiff.js';
+import genDiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
     console.log(genDiff(filepath1, filepath2, options.format));
