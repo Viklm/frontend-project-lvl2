3478524c311b6077671e04b912ad58e6f9ddf138
Viktor <vik333444@yandex.ru>
2021-10-05 16:07:52 +0400

    fix linters errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,7 +10,7 @@ const makePlain = (difference) => {
   const make = (object, parent = '') => {
     switch (object.status) {
       case 'nested':
-        return  object.descendants.map((element) => make(element, `${parent + object.key}.`)).filter((elem) => elem !== null).join('\n');
+        return object.descendants.map((element) => make(element, `${parent + object.key}.`)).filter((elem) => elem !== null).join('\n');
       case 'changed':
         return `Property '${parent}${object.key}' was ${object.status}. From ${getValue(object.value1)} to ${getValue(object.value2)}`;
       case 'deleted':
@@ -18,7 +18,7 @@ const makePlain = (difference) => {
       case 'added':
         return `Property '${parent}${object.key}' was ${object.status} with value: ${getValue(object.value)}`;
       case 'unchanged':
-        return null; 
+        return null;
     }
   };
   return `${difference.map((object) => make(object)).join('\n')}`;
